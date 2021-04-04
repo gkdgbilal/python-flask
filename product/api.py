54bfeed7a86ebb63242bd9d206f685db7f5ca36d
bilal <gkdgbilal>
2021-04-04 12:11:12 +0300

    process is done.

---
+++ product/api.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Product(Resource):
+    def get(self):
+        return 'Hello World from Flask!'
+
+api.add_resource(Product, '/staj')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
