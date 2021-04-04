ba2bbd4f5dff0c6908c95b4d8e1269f741ae7dbd
bilal <gkdgbilal>
2021-04-04 15:59:59 +0300

    latest version

--- flask/api.py
+++ flask/api.py
@@ -4,11 +4,11 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Product(Resource):
+class Flask(Resource):
     def get(self):
         return 'Hello World from Flask!'
 
-api.add_resource(Product, '/staj')
+api.add_resource(Flask, '/staj')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=4444, debug=True)
